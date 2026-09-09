18e05182c69aeab74bca3b18d6ab8bb329e958cc
Yashveer Singh Rajpoot <yashveer@obrive.com>
2026-09-09 16:11:20 +0530

    feat: Implement Gmail OAuth2 authentication for nodemailer

--- src/app/api/get-in-touch/route.js
+++ src/app/api/get-in-touch/route.js
@@ -48,8 +48,11 @@ export async function POST(request) {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
+                type: 'OAuth2',
                 user: process.env.EMAIL,
-                pass: process.env.PASSWORD,
+                clientId: process.env.OAUTH_CLIENT_ID,
+                clientSecret: process.env.OAUTH_CLIENT_SECRET,
+                refreshToken: process.env.OAUTH_REFRESH_TOKEN,
             },
         })
 
